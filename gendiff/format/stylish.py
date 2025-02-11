a4fee8d87638586aedc27efc7ba725b1584cda7d
Peter_007 <hihb3ya@gmail.com>
2025-02-11 20:07:59 +0300

    some changes

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -29,35 +29,40 @@ def format_stylish(diff, depth=1):
 
         elif typ == "unchanged":
             value = stringify(item["value"], depth + 1)
-            lines.append(
-                f"{indent_for_unchanged}{key}: " + (value if value else "")
-            )
+            lines.append(f"{indent_for_unchanged}{key}: {value}")
 
         elif typ == "changed":
             old_value = stringify(item["old_value"], depth + 1)
             new_value = stringify(item["new_value"], depth + 1)
-
-            # Старое значение
-            lines.append(
-                f"{indent_for_marker}- {key}: " +
-                (old_value if old_value else "")
+            line_old = (
+                f"{indent_for_marker}- {key}:"
+                if old_value == ""
+                else f"{indent_for_marker}- {key}: {old_value}"
             )
-            # Новое значение
-            lines.append(
-                f"{indent_for_marker}+ {key}: " +
-                (new_value if new_value else "")
+            line_new = (
+                f"{indent_for_marker}+ {key}:"
+                if new_value == ""
+                else f"{indent_for_marker}+ {key}: {new_value}"
             )
+            lines.append(line_old)
+            lines.append(line_new)
 
         elif typ == "removed":
             value = stringify(item["value"], depth + 1)
-            lines.append(
-                f"{indent_for_marker}- {key}: " + (value if value else "")
+            line_removed = (
+                f"{indent_for_marker}- {key}:"
+                if value == ""
+                else f"{indent_for_marker}- {key}: {value}"
             )
+            lines.append(line_removed)
 
         elif typ == "added":
             value = stringify(item["value"], depth + 1)
-            lines.append(
-                f"{indent_for_marker}+ {key}: " + (value if value else "")
+            line_added = (
+                f"{indent_for_marker}+ {key}:"
+                if value == ""
+                else f"{indent_for_marker}+ {key}: {value}"
             )
+            lines.append(line_added)
 
     return "\n".join(["{"] + lines + [f"{bracket_indent}}}"])
