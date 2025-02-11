2092b13dce8e9db6bef765ba3015ccfafa9b95c6
Peter_007 <hihb3ya@gmail.com>
2025-02-11 19:29:19 +0300

    some changes

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -32,25 +32,33 @@ def format_stylish(diff, depth=1):
         elif typ == "changed":
             old_value = stringify(item["old_value"], depth + 1)
             new_value = stringify(item["new_value"], depth + 1)
-            line1 = (f"{indent_for_marker}- {key}:" if old_value == ""
-                     else f"{indent_for_marker}- {key}: {old_value}")
-            line2 = (f"{indent_for_marker}+ {key}:" if new_value == ""
-                     else f"{indent_for_marker}+ {key}: {new_value}")
+            line1 = (
+                f"{indent_for_marker}- {key}:"
+                if old_value == ""
+                else f"{indent_for_marker}- {key}: {old_value}"
+            )
+            line2 = (
+                f"{indent_for_marker}+ {key}:"
+                if new_value == ""
+                else f"{indent_for_marker}+ {key}: {new_value}"
+            )
             lines.append(line1)
             lines.append(line2)
         elif typ == "removed":
             value = stringify(item["value"], depth + 1)
-            if value == "":
-                line = f"{indent_for_marker}- {key}: "
-
-            else:
-                line = f"{indent_for_marker}- {key}: {value}"
-
+            line = (
+                f"{indent_for_marker}- {key}:"
+                if value == ""
+                else f"{indent_for_marker}- {key}: {value}"
+            )
             lines.append(line)
         elif typ == "added":
             value = stringify(item["value"], depth + 1)
-            line = (f"{indent_for_marker}+ {key}:"
-                    if value == "" else f"{indent_for_marker}+ {key}: {value}")
+            line = (
+                f"{indent_for_marker}+ {key}:"
+                if value == ""
+                else f"{indent_for_marker}+ {key}: {value}"
+            )
             lines.append(line)
 
     return "\n".join(["{"] + lines + [f"{bracket_indent}}}"])
