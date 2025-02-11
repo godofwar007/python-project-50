a411826ff6034c262e7b54a8431cfac731eab817
Peter_007 <hihb3ya@gmail.com>
2025-02-11 20:47:28 +0300

    some changes

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -1,15 +1,21 @@
 def stringify(value, depth):
+    """Преобразует значение в строку с учётом вложенных структур."""
     if isinstance(value, dict):
         indent = ' ' * (depth * 4)
         closing_indent = ' ' * ((depth - 1) * 4)
         lines = [
-            f"{indent}{k}: "
-            f"{stringify(v, depth + 1)}" for k, v in value.items()]
+            f"{indent}{k}: {stringify(v, depth + 1)}" for k, v in value.items()]
         return "{\n" + "\n".join(lines) + "\n" + closing_indent + "}"
-    if isinstance(value, bool):
-        return str(value).lower()
+
     if value is None:
         return "null"
+
+    if isinstance(value, bool):
+        return str(value).lower()
+
+    if value == "":  # Убираем кавычки для пустых строк
+        return ""
+
     return str(value)
 
 
@@ -34,15 +40,23 @@ def format_stylish(diff, depth=1):
         elif typ == "changed":
             old_value = stringify(item["old_value"], depth + 1)
             new_value = stringify(item["new_value"], depth + 1)
-            lines.append(f"{indent_for_marker}- {key}: {old_value}".rstrip())
-            lines.append(f"{indent_for_marker}+ {key}: {new_value}".rstrip())
+            old_line = f"{indent_for_marker}- {key}:" + \
+                (f" {old_value}" if old_value != "" else "")
+            new_line = f"{indent_for_marker}+ {key}:" + \
+                (f" {new_value}" if new_value != "" else "")
+            lines.append(old_line)
+            lines.append(new_line)
 
         elif typ == "removed":
             value = stringify(item["value"], depth + 1)
-            lines.append(f"{indent_for_marker}- {key}: {value}".rstrip())
+            line = f"{indent_for_marker}- {key}:" + \
+                (f" {value}" if value != "" else "")
+            lines.append(line)
 
         elif typ == "added":
             value = stringify(item["value"], depth + 1)
-            lines.append(f"{indent_for_marker}+ {key}: {value}".rstrip())
+            line = f"{indent_for_marker}+ {key}:" + \
+                (f" {value}" if value != "" else "")
+            lines.append(line)
 
     return "\n".join(["{"] + lines + [f"{bracket_indent}}}"])
